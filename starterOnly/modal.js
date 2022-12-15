d500504402afbdafe3aea5e17d177be998c8ae89
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2022-12-15 09:40:24 +0100

    Close Modal Ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelector(".close");
+
+
+console.log(modalBtnClose);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtnClose.addEventListener("click", closeModal);
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+function closeModal() {
+  modalbg.style.display = "none";
+}
